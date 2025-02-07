256241a63cb0695ddede17756476048dc71c5497
RANIT <sranit806@gmail.com>
2025-02-07 12:15:46 +0530

    code filter of Profile_Ranit

--- PORTFOLIO/PORTFOLIO/script.js
+++
@@ -1,9 +0,0 @@
-
-function openNewTab() {
-  var selectElement = document.getElementById('redirect-dropdown');
-  var selectedValue = selectElement.value;
-  
-  if (selectedValue) {
-    window.open(selectedValue, '_blank');
-  }
-}
